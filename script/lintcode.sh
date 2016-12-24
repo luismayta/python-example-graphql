4abd1bcafe52693160c3021f58cb5958154c7918
@slovacus <slovacus@gmail.com>
2016-12-23 22:18:53 -0500

    [#3] Fix: settings script

--- script/lintcode.sh
+++ script/lintcode.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-# shellcheck source=script/bootstrap.sh
-# shellcheck disable=SC1091
+# shellcheck source=/dev/null
 [ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
 
 pre-commit install -f --install-hooks
--- script/sync.sh
+++ script/sync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
 
-# shellcheck source=script/bootstrap.sh
+# shellcheck source=/dev/null
 [ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
