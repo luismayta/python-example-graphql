ccf3dabbfaf575ffa895c70b7eefd223d8d0cb25
@slovacus <slovacus@gmail.com>
2017-05-07 08:46:46 -0500

    !#2 Add: class model comments

--- src/comments/models.py
+++ src/comments/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from posts.models import Post
+
 
 # Create your models here.
+class Comment(models.Model):
+    """Documentation for ClassName
+
+    """
+    post = models.ForeignKey(Post)
+    text = models.CharField(max_length=200)
+    author = models.CharField(max_length=20)
+
+    def __unicode__(self):
+        return self.text
